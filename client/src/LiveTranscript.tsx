51cd167b66b8d7145e4ac5c73a37abf30e2dde06
Donald Ubuntu <donaldptngai@gmail.com>
2023-08-18 20:49:12 -0400

    added divs back in

--- client/src/LiveTranscript.tsx
+++ client/src/LiveTranscript.tsx
@@ -72,17 +72,17 @@ const LiveTranscriptApp = () => {
         removeSilence: true,
     })
 
-    // return (
-    // <div>
-    //     <p>Recording: {recording}</p>
-    //     <p>Speaking: {speaking}</p>
-    //     <p>Transcribing: {transcribing}</p>
-    //     <p>Transcribed Text: {transcript.text}</p>
-    //     <button onClick={() => startRecording()}>Start</button>
-    //     <button onClick={() => pauseRecording()}>Pause</button>
-    //     <button onClick={() => stopRecording()}>Stop</button>
-    // </div>
-    // )
+    return (
+        <div>
+            <p>Recording: {recording}</p>
+            <p>Speaking: {speaking}</p>
+            <p>Transcribing: {transcribing}</p>
+            <p>Transcribed Text: {transcript.text}</p>
+            <button onClick={() => startRecording()}>Start</button>
+            <button onClick={() => pauseRecording()}>Pause</button>
+            <button onClick={() => stopRecording()}>Stop</button>
+        </div>
+    )
 }
 
 export default LiveTranscriptApp;
