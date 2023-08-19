5ebb5dc78c40b33f2363175d78f697949cabe7f9
Donald Ubuntu <donaldptngai@gmail.com>
2023-08-19 12:14:01 -0400

    understanding logic a bit more for blueprints

--- backend/Blueprints/transcriptApi/__init__.py
+++ backend/Blueprints/transcriptApi/__init__.py
@@ -8,10 +8,12 @@ def index():
 
 @transcript_api_bp.route("/add/<int:num1>/<int:num2>")
 def add(num1, num2):
-    return str(num1 + num2)
+    return addTwo(num1,num2)
 
 @transcript_api_bp.route("/go_to_main")
 def go_to_main():
     return redirect(url_for("main.index")) # main, and then function name which is index in routes.py
 
 
+def addTwo(int1, int2):
+    return str(int1 + int2)
